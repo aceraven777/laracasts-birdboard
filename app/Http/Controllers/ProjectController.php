58315525a2fd148e90492c6ab4b860201d4c0bc4
Aris Lacdao <aris.lacdao@quiddity.ph>
2020-01-13 18:00:34 +0800

    Episode 2 > Creating a project

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Project list
+     *
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory
+     */
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+
+    /**
+     * Create Project
+     *
+     * @param Request $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        Project::create($request->only(['title', 'description']));
+
+        return redirect('/projects');
+    }
+}
