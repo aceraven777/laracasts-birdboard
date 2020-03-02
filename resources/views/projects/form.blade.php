43934b5f6e9dcd07927284793c77b255a1b3be83
Aris Lacdao <aris.lacdao@quiddity.ph>
2020-03-02 11:20:59 +0800

    Episode 36 > Easy Tailwind Themes Using CSS Variables

--- resources/views/projects/form.blade.php
+++ resources/views/projects/form.blade.php
@@ -1,4 +1,3 @@
-
 @csrf
 
 <div class="field mb-6">
@@ -6,13 +5,12 @@
 
     <div class="control">
         <input
-            type="text"
-            class="input bg-transparent border border-grey-light rounded p-2 text-xs w-full"
-            name="title"
-            placeholder="My next awesome project"
-            required
-            value="{{ $project->title }}"
-        >
+                type="text"
+                class="input bg-transparent border border-muted-light rounded p-2 text-xs w-full"
+                name="title"
+                placeholder="My next awesome project"
+                required
+                value="{{ $project->title }}">
     </div>
 </div>
 
@@ -20,13 +18,12 @@
     <label class="label text-sm mb-2 block" for="description">Description</label>
 
     <div class="control">
-        <textarea
-            name="description"
-            rows="10"
-            class="textarea bg-transparent border border-grey-light rounded p-2 text-xs w-full"
-            placeholder="I should start learning piano."
-            required
-        >{{ $project->description }}</textarea>
+            <textarea
+                name="description"
+                rows="10"
+                class="textarea bg-transparent border border-muted-light rounded p-2 text-xs w-full"
+                placeholder="I should start learning piano."
+                required>{{ $project->description }}</textarea>
     </div>
 </div>
 
@@ -34,14 +31,8 @@
     <div class="control">
         <button type="submit" class="button is-link mr-2">{{ $buttonText }}</button>
 
-        <a href="{{ $project->path() }}">Cancel</a>
+        <a href="{{ $project->path() }}" class="text-default">Cancel</a>
     </div>
 </div>
 
-@if ($errors->any())
-    <ul class="field mt-6">
-        @foreach ($errors->all() as $error)
-            <li class="text-sm text-red">{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
+@include ('errors')
